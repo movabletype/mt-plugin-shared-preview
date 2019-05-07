08c8ef71a57184deafe03e6b3b60b918c272f43b
mchino <mchino@sixapart.com>
2019-05-07 10:09:01 +0900

    Delete display switching script. MTC-26580

--- plugins/SharedPreview/lib/SharedPreview/CMS/SharedPreviewBase.pm
+++ plugins/SharedPreview/lib/SharedPreview/CMS/SharedPreviewBase.pm
@@ -18,21 +18,9 @@ sub add_shared_preview_link {
     my ( $class, $href ) = @_;
     my $type           = $class->{type};
     my $link           = $class->{href} . $href;
-    my $release_status = MT::Entry::RELEASE();
 
     return <<"__JS__";
-jQuery('button[name=preview_$type]').after('<div id="shared_preview" style="display:none;" class="text-right"><a href="$link">Shared Preview</a></div>');
-    jQuery('select[name=status]').on('change', function(){
-        switch_display_preview();
-    });
-    function switch_display_preview() {
-        if (jQuery('select[name=status]').val() != $release_status) {
-            jQuery("#shared_preview").show();
-        } else {
-            jQuery("#shared_preview").hide();
-        }
-    }
-    switch_display_preview();
+jQuery('button[name=preview_$type]').after('<div id="shared_preview" class="text-right"><a href="$link">Shared Preview</a></div>');
 __JS__
 }
 
