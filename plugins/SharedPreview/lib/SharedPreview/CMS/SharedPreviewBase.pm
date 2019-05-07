24048697607944809c584dd0104268995234a636
mchino <mchino@sixapart.com>
2019-05-07 13:53:06 +0900

    Change link display logic from JavaScript to perl. MTC-26580

--- plugins/SharedPreview/lib/SharedPreview/CMS/SharedPreviewBase.pm
+++ plugins/SharedPreview/lib/SharedPreview/CMS/SharedPreviewBase.pm
@@ -18,20 +18,9 @@ sub add_shared_preview_link {
     my ( $class, $href ) = @_;
     my $type = $class->{type};
     my $link = $class->{href} . $href;
-    my $release_status = MT::Entry::RELEASE();
 
     return <<"__JS__";
 jQuery('button[name=preview_$type]').after('<div id="shared_preview" class="text-right"><a href="$link">Shared Preview</a></div>');
-
-    function switch_display_preview() {
-        if (jQuery('input[name=old_status]').val() != $release_status) {
-            jQuery("#shared_preview").show();
-        } else {
-            jQuery("#shared_preview").hide();
-        }
-    }
-
-    switch_display_preview();
 __JS__
 }
 
