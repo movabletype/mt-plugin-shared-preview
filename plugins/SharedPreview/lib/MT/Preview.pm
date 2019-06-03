d5844aa5fc33adef2bb89aadc0ffd15887c2da5d
mchino <mchino@sixapart.com>
2019-06-03 13:40:24 +0900

    Fix page share preview link.MTC-26685

--- plugins/SharedPreview/lib/MT/Preview.pm
+++ plugins/SharedPreview/lib/MT/Preview.pm
@@ -119,8 +119,6 @@ sub purge {
 sub shared_preview_link {
     my ( $type, $href ) = @_;
 
-    $type = 'entry' if $type eq 'page';
-
     return <<"__JS__";
 jQuery('button[name=preview_$type]').after('<div id="shared_preview" class="text-right"><a href="$href">Shared Preview</a></div>');
 __JS__
